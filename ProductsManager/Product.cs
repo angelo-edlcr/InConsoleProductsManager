ed3f42c556a178cb23547ac318f388bb99a1626a
Angelo De La Cruz <99463079+angelo-edlcr@users.noreply.github.com>
2022-10-23 17:02:14 -0400

    First upload
    Save in file.
    Create, read and delete products.
    Update product module is in process.

---
+++ ProductsManager/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsManager
+{
+    public class Product
+    {
+        //public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(string code, string name, string category, double price, int stock)
+        {
+            //Id = id;
+            Code = code;
+            Name = name;
+            Category = category;
+            Price = price;
+            Stock = stock;
+        }
+
+        // Override the ToString() method to a format for save it in the .txt file
+        public override string ToString()
+        {
+            return $"{Code}|{Name}|{Category}|{Price}|{Stock}";
+        }
+    }
+}
